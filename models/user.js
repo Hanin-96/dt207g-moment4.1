7e2fbc99ce30ab5287a0944b8a71467d0f7f1456
Hanin Farhan <haninfarhan96@gmail.com>
2024-05-08 11:34:56 +0200

    registrering av ny användare och hashning av lösenord

--- models/user.js
+++ models/user.js
@@ -38,6 +38,34 @@ const userSchema = new mongoose.Schema({
         default: Date.now
     }
 });
-
 const user = mongoose.model("user", userSchema);
+
+
+//Hasha lösenord innan den sparas
+userSchema.pre("save", async function(next) {
+    try {
+        if (this.isNew || this.isModified("password")) {
+            const hashedPassword = await bcrypt.hash(this.password, 10);
+            this.password = hashedPassword;
+        }
+        next();
+
+    } catch (error) {
+        next(error);
+    }
+});
+
+//Registrera ny user
+userSchema.statics.register = async function(username, firstname, lastname, email, password) {
+    try {
+        const user = new this({ username, firstname, lastname, email, password });
+        await user.save();
+        return user;
+
+    } catch (error) {
+        throw error;
+    }
+};
+
+
 module.exports = user;
